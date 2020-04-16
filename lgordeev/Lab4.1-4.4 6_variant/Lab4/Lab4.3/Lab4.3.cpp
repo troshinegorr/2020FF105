391820c9d54939b8b5bbc0dd0c88353349cb3876
Pridira <63130495+Pridira@users.noreply.github.com>
2020-04-16 16:25:21 +0500

    Create Lab4.3.cpp

---
+++ lgordeev/Lab4.1-4.4 6_variant/Lab4/Lab4.3/Lab4.3.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int p(int n)
+{
+    int res = 1;
+    for (int i = n + 1; i <= 2 * n; i++) {
+        res *= i;
+    }
+    return res;
+}
+
+void main()
+{
+    setlocale(LC_ALL, "ru");
+    int N;
+    double a, sum, e;
+    N = 0;
+    a = 1.0 / p(N);
+    sum = 0;
+    cout << "Введите число e ";
+    cin >> e;
+    while (a >= e) {
+        sum += a;
+        N++;
+        a = 1.0 / p(N);
+    }
+    cout << "Сумма чисел = " << sum << endl;
+
+    system("pause");
+}
