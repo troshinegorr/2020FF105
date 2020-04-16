2120c1d3fd18cfa1a10a9c3133cb9f190db5b9c7
Pridira <63130495+Pridira@users.noreply.github.com>
2020-04-16 16:24:55 +0500

    Create Lab4.2.cpp

---
+++ lgordeev/Lab4.1-4.4 6_variant/Lab4/Lab4.2/Lab4.2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+# include <math.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int n = 0;
+    double x, S = 0, a;
+    cout << "Введите x и n ";
+    cin >> x >> n;
+    for (int i = 0; i < n; i++) {
+        a = sin(x);
+        for (int j = 0; j < i; j++) {
+            a = sin(a);
+        }
+        S = S + a;
+    }
+    cout << "S = " << S;
+    
+    return 0;
+}
