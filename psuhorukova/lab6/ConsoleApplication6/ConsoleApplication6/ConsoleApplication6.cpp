86829eee41369d59998790b3d963bb2233b45134
suhorucova <63137041+suhorucova@users.noreply.github.com>
2020-04-19 20:05:31 +0500

    lab6_A_B

---
+++ psuhorukova/lab6/ConsoleApplication6/ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+double dlina(double x1, double y1,double x2, double y2)
+{
+    double a;
+    a = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    return a;
+}
+
+int main()
+{
+    setlocale(0, "");
+    double x1, x2, x3, y1, y2, y3;
+    cout << "Введите координаты вершин труегольника: x1 ";cin >> x1; 
+    cout << "y1 "; cin >> y1; 
+    cout << "x2 "; cin >> x2; cout << "y2 "; cin >> y2;
+    cout << "x3 "; cin >> x3; cout << "y3 "; cin >> y3;
+    double a, b, c;
+    a = dlina(x1, y1, x2, y2);
+    b = dlina(x2, y2, x3, y3);
+    c = dlina(x3, y3, x1, y1);
+    double p;
+    p = (a + b + c) / 2;
+    double s = sqrt(p * (p - a) * (p - b) * (p - c));
+    cout << "Площадь треугольника: " << s << endl;
+    cin.get(); cin.get();
+    return 0;
+}
+
---
+++ psuhorukova/lab6/ConsoleApplication6_2/ConsoleApplication6_2/ConsoleApplication6_2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+
+int period(int n) 
+{
+    int arr[1000];
+    int i = 0;
+    int k = 10;
+    bool a=true;
+    do {
+        arr[i] = k / n;
+        k = (k - n * (k / n)) * 10;
+        for (int p = 0; p < i; p++)
+        {
+            if (arr[p]==arr[i])
+            {
+                a = false;
+                break;
+            }
+        }
+        if (a == true) { i++; }
+    } while (a == true);
+    for (int t = 0; t < i; t++)
+    {
+        if (arr[t]==arr[i])
+        {
+            i = i - t;
+            break;
+        }
+    }
+    return i;
+}
+int main()
+{
+   setlocale(0, "");
+   int n;
+   cout << "Введите n (n>1): ";
+   cin >> n;
+   int b;
+   b = period(n);
+   cout << "Длина периода: " << b << endl;
+   cin.get(); cin.get();
+   return 0;
+}
+
