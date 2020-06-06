6870db5574df944a6c7b9bc2d5ccb2ebe0dd09d1
suhorucova <63137041+suhorucova@users.noreply.github.com>
2020-06-06 16:07:50 +0500

    lab_9

---
+++ psuhorukova/lab9/ConsoleApplication_9/ConsoleApplication_9/ConsoleApplication_9.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    string slovo;
+    cout << "Введите слово: ";
+    cin >> slovo;
+    int a;
+    a = slovo.length();
+    int b = 0;
+    for (int i = 0; i < a/2; i++)
+    {
+        if (slovo[i]!=slovo[a-1-i])
+            {
+             b=b+1;
+            }
+    }
+    if (b==0)
+    {
+        cout << "Слово - палиндром" << endl;
+    }
+    else {
+       cout << "Слово не палиндром" << endl;
+    }
+    
+    cin.get(); cin.get();
+    return 0;
+}
+
+
+
