d751f501840db8b3b532d39da6b9d8c10b316fac
Pridira <63130495+Pridira@users.noreply.github.com>
2020-06-15 02:16:15 +0500

    Lab_9

---
+++ lgordeev/Lab_9/Lab_9/Lab_9.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int res = 0;
+	string str;
+	cout << "Введите строку: ";
+	getline(cin, str);
+	for (int i = 0; str[i] != ':'; i++) {
+		res++;
+	}
+	cout << "Число символов перед двоеточием: " << res;
+}
