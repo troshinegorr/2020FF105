e2606b53cf9dca88594f99eefce6d30ef89b93f8
Кристина Куракина <kristina.kurakina@csu.ru>
2020-06-27 22:16:42 +0500

    Kurakina 1lab

---
+++ kkurakina/1 лаба/1.2яп/1.2яп/1.2яп.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include<math.h>
+using namespace std;
+int main(void)
+{
+	double a, d, n;
+	float S;
+	setlocale(LC_ALL, "Ru");
+	cout << "Введите значение a d n ";
+	cin >> a >> d>>n;
+	S = ((2 * a + d * (n - 1)) / 2) * n;
+	cout << "Ответ " << S;
+
+}
---
+++ kkurakina/1 лаба/1.4яп/1.4яп/1.4яп.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include<math.h>
+using namespace std;
+int main(void)
+{
+	double x, y, z;
+	setlocale(LC_ALL, "Ru");
+	cout << "Введите значение x,y ";
+	cin >> x>>y;
+	z = 0;
+	if ((x >= 2 && x <= 6) && (y >= 2 && y <= 5))
+	{
+		z = 1;
+	}
+	if ((x >= -2 && x <= 2) && (y >= -5 && y <= 5))
+	{
+		z = 1;
+	}
+	if ((x >= -6 && x <= -2) && (y >= 2 && y <= 5))
+	{
+		z = 1;
+	}
+	cout << z;
+}
