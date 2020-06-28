0161102efbfed2a8f51c8f9bb4ca8fa35e01bb59
Кристина Куракина <kristina.kurakina@csu.ru>
2020-06-28 21:05:38 +0500

    Kurakina 6lab

---
+++ kkurakina/6 лаба/6.3яп/6.3яп/6.3яп.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int MinK(int* X, int k)
+
+{
+
+	int R, Result = X[k - 1];
+
+	if (k == 1)
+
+		Result = X[0];
+
+	if (k > 1)
+
+	{
+
+		R = MinK(X, k - 1);
+
+		if (Result > R) Result = R;
+
+	}
+
+	return Result;
+
+}
+
+int main()
+
+{
+
+	int X[100], n, i;
+
+	setlocale(LC_CTYPE, "Russian");
+
+	printf("Введите количество элементов в массиве: ");
+
+	cin >> n;
+
+	for (i = 0; i < n; i++)
+
+	{
+
+		printf("\nX[%d]=", i);
+
+		cin >> X[i];
+
+	}
+
+	printf("\nMin(X) = %d\n", MinK(X, n));
+
+}
