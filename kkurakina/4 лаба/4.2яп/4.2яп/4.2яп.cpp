479c168eaf7a692ca16689d4ffef2e316993e2aa
Кристина Куракина <kristina.kurakina@csu.ru>
2020-06-28 18:42:31 +0500

    Kurakina 4lab

---
+++ kkurakina/4 лаба/4.2яп/4.2яп/4.2яп.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+#include <stdlib.h>
+
+#include <math.h>
+
+#include <locale>
+
+using namespace std;
+
+int main()
+
+{
+
+	setlocale(LC_ALL, "Russian");
+
+	int y, n, i;
+
+	cin >> n;
+
+	y = 1;
+
+	for (i = 1; i <= n; i++)
+
+		y = y * (i * 2 - 1);
+
+	cout << y << endl;
+
+	system("pause");
+
+}
---
+++ kkurakina/4 лаба/4.4яп/4.4яп/4.4яп.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+#include <clocale>
+
+#include <cmath>
+
+using namespace std;
+
+int main()
+
+{
+
+	setlocale(LC_ALL, "Russian");
+
+	double a, b, f, h;
+
+	cout << "Введите левую границу интервала a = ";
+
+	cin >> a;
+
+	cout << "Введите правую границу интервала b = ";
+
+	cin >> b;
+
+	cout << "Введите шаг h = ";
+
+	cin >> h;
+
+	for (double i = a; i <= b; i += h)
+
+	{
+
+		f = 0.5 * (cos(i / 4) / sin(i / 4)) + 4;
+
+		cout << "x = " << i << "\tf(x) = " << f << endl;
+
+	}
+
+	system("pause");
+
+	return 0;
+
+}
