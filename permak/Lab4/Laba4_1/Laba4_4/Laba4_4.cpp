149a2cd581b1eaf967e5aaea41843f010fe3a589
ErmakPavel <67833446+ErmakPavel@users.noreply.github.com>
2020-07-06 02:55:50 +0500

    Lab4

---
+++ permak/Lab4/Laba4_1/Laba4_4/Laba4_4.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <clocale>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double a, b, f, h;
+	cout << "Введите левую границу интервала a = ";
+	cin >> a;
+	cout << "Введите правую границу интервала b = ";
+	cin >> b;
+	cout << "Введите шаг h = ";
+	cin >> h;
+	for (double i = a; i <= b; i += h)
+	{
+		f = sqrt(i) * pow(cos(i), 2);
+		cout << "x = " << i << "\tf(x) = " << f << endl;
+	}
+	system("pause");
+	return 0;
+}
