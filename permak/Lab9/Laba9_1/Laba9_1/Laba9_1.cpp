9f581198239c1d3b2b278b2e30e2232c151e44fb
ErmakPavel <67833446+ErmakPavel@users.noreply.github.com>
2020-07-06 03:39:09 +0500

    Lab9

---
+++ permak/Lab9/Laba9_1/Laba9_1/Laba9_1.cpp
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char str[256];
+	int cnt = 0, cnt1 = 0;
+	printf("Enter string -> ");
+	cin >> str;
+	printf("\nSymbols -> ");
+	for (int i = 0; i < strlen(str); i++)
+	{
+		cnt1 = 0;
+		for (int j = 0; j < strlen(str); j++)
+		{
+			if (str[i] == str[j])
+			{
+				cnt1++;
+			}
+		}
+		if (cnt1 == 1)
+		{
+			printf("%c ", str[i]);
+			cnt++;
+		}
+	}
+	printf("\nNumber symbol -> %d", cnt);
+	return 0;
+}
