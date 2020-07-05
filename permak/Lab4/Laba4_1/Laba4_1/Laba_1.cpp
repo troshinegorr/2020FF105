da0ea2c63c4ff2c51e6be70448035ebd0c8a0874
ErmakPavel <67833446+ErmakPavel@users.noreply.github.com>
2020-07-06 01:40:37 +0500

    Lab4

---
+++ permak/Lab4/Laba4_1/Laba4_1/Laba_1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+void primeGenerator(int x)
+
+{
+	if (x < 1 || x > 36)
+		cout << "Invalid value of x!";
+	else
+		for (int i = 0; i <= x; ++i)
+			cout << (pow(2, 2 * i + 1) + 1) / 3 << endl;
+}
+
+int main()
+
+{
+	int x;
+	cout << "Enter x ";
+	cin >> x;
+	primeGenerator(x);
+	return 0;
+}
