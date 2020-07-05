9efc567dbdb305128260fd61156cf3a400d1cc94
ErmakPavel <67833446+ErmakPavel@users.noreply.github.com>
2020-07-06 03:20:19 +0500

    Lab6

---
+++ permak/Lab6/Laba6_1/Laba6_1/Laba6_1.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <cmath>
+#include <process.h>
+
+using namespace std;
+
+class fraction
+{
+private:
+	int chis;
+	int znam;
+public:
+	fraction() : chis(0), znam(1)
+	{ }
+	fraction(int ch, int zn) : chis(ch), znam(zn)
+	{ }
+	void getfrac()
+	{
+		cout << "Введите числитель: "; cin >> chis;
+		while (true)
+		{
+			cout << "Введите знаменатель: "; cin >> znam;
+			if (znam == 0)
+				cout << "Попытка деления на 0!!!\n";
+			else
+				break;
+		}
+	}
+
+	void showfrac() const
+	{
+		cout << chis << '/' << znam;
+	}
+	fraction operator / (const fraction& f1) const
+	{
+		int chtemp = chis * f1.znam;
+		int zntemp = znam * f1.chis;
+		return lowterms(fraction(chtemp, zntemp));
+	}
+	fraction lowterms(fraction) const;
+};
+
+fraction fraction::lowterms(fraction f1) const
+{
+	int chtemp, zntemp, temp;
+	chtemp = abs(f1.chis);
+	zntemp = abs(f1.znam);
+	if (zntemp == 0)
+	{
+		cout << "\nОшибка! Попытка деления на 0!!!\n";
+		exit(1);
+	}
+	if (chtemp == 0)
+	{
+		return fraction(0, 1);
+	}
+	while (chtemp != 0)
+	{
+		if (chtemp < zntemp)
+		{
+			temp = chtemp; chtemp = zntemp; zntemp = temp;
+		}
+		chtemp -= zntemp;
+	}
+	return fraction(f1.chis / zntemp, f1.znam / zntemp);
+}
+
+int main()
+
+{
+	setlocale(LC_ALL, "Rus");
+	fraction f1, f2, f3;
+	cout << "Введите дробь f1:\n";
+	f1.getfrac();
+	cout << "Введите дробь f2:\n";
+	f2.getfrac();
+	f3 = f1 / f2;
+	cout << "Результат деления f1 на f2: "; f3.showfrac();
+	cout << endl;
+	return 0;
+}
---
+++ permak/Lab6/Laba6_1/Laba6_2/Laba6_2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int Factorial(int f) 
+{
+	int i = 1;
+	int ans = 1;
+	if (f > 0) 
+	{
+		for (; i <= f; i++) 
+		{
+			ans *= i;
+		}
+		return ans;
+	}
+
+	else 
+	{
+		return 1;
+	}
+
+}
+
+int main() {
+	int a;
+	int s = 0;
+	int j;
+	cin >> a;
+	for (j = 1; j <= a; j++)
+		s = s + Factorial(j);
+	cout << s << endl;
+	return 0;
+}
