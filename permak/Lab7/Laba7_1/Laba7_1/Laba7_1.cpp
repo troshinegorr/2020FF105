f8701415356c96aee2b63e192f36902ee665ecec
ErmakPavel <67833446+ErmakPavel@users.noreply.github.com>
2020-07-06 03:27:37 +0500

    Lab7

---
+++ permak/Lab7/Laba7_1/Laba7_1/Laba7_1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	setlocale(LC_ALL, "Russian");
+	int a[100];
+	cin >> N;
+	cout << "Введите элементы целочисленного массива: \n";
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> a[i];
+	}
+	cout << "Получен целочисленный массив с элементами: \n";
+	for (int i = 1; i <= N; i++)
+	{
+		if (a[i] > i)
+			cout << "a[" << i << "]=" << a[i] << ' ';
+	}
+	system("pause");
+	return 0;
+}
---
+++ permak/Lab7/Laba7_1/Laba7_2/Laba7_2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	int min = 9999999;
+	setlocale(LC_ALL, "Russian");
+	int a[100];
+	cin >> N;
+	cout << "Введите элементы целочисленного массива: \n";
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> a[i];
+	}
+	cout << "Получен целочисленный массив с элементами: \n";
+	for (int i = 1; i <= N; i++)
+	{
+		if (a[i] < min)
+			min = a[i];
+	}
+	for (int i = 1; i <= N; i++)
+	{
+		if (a[i] != min)
+			cout << a[i] << endl;
+	}
+	system("pause");
+	return 0;
+}
