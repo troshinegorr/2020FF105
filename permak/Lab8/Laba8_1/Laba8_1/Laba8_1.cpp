c2b695ea8a3157ca4cd529ba74cb74a1ffa4ca3c
ErmakPavel <67833446+ErmakPavel@users.noreply.github.com>
2020-07-06 03:31:06 +0500

    Lab8

---
+++ permak/Lab8/Laba8_1/Laba8_1/Laba8_1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, k;
+	int a[100][100] = { 0 };
+	cin >> n;
+	k = n;
+	for (int i = 1; i <= n; ++i)
+		a[i][i] = i;
+	for (int i = 1; i <= n; ++i)
+	{
+		a[i][n - i + 1] = k;
+		k--;
+	}
+
+	for (int i = 1; i <= n; ++i)
+	{
+		cout << endl;
+		for (int j = 1; j <= n; ++j)
+			cout << a[i][j] << " ";
+	}
