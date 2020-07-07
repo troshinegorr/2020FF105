78ee80892fe17aa6b71fd01284678a2540c73241
Vika0413 <67801637+Vika0413@users.noreply.github.com>
2020-07-07 23:42:54 +0500

    Add files via upload

---
+++ vmotovilova/7.1/7.1/7.1.cpp
@@ -0,0 +1,43 @@
+﻿// 7.1
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale (LC_ALL, "Russian");
+    const int N = 9; //Число элементов массива
+    int arr[N];//Сам массив
+    int occ[N];//Массив, хранящий позиции вхождений
+    int s = 0;
+
+    for (int i = 0; i < N; ++i)
+    {
+        do
+        {
+            cout << "Введите элемент №" << i + 1 << ": ";
+            cin >> arr[i];
+        } 
+        
+        while ((arr[i] > 9) || (arr[i] < 0));
+    }
+
+    for (int i = 0; i < N - 2; ++i)
+        if ((arr[i] == 1) && (arr[i + 1] == 2) && (arr[i + 2] == 3))
+        {
+            occ[s] = i;
+            ++s;
+        }
+
+    if (s == 0)
+        cout << "Вхождений не найдено" << endl;
+    else
+    {
+        cout << "Найдено " << s << " вхождений:" << endl;
+        for (int i = 0; i < s; ++i)
+            cout << occ[i] << endl;
+    }
+
+    system("pause");
+    return 0;
+}
---
+++ vmotovilova/9.1/9.1/9.1.cpp
@@ -0,0 +1,22 @@
+﻿// 9.1
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+int main() {
+    std::string str, wrd, tmp;
+    std::cout << "String: ";
+    std::getline(std::cin, str);
+    std::cout << "Word: ";
+    std::cin >> wrd;
+
+    std::istringstream ist(str);
+    size_t cnt = 0;
+    while (ist >> tmp)
+        if (tmp == wrd)
+            ++cnt;
+    std::cout << "meet " << cnt << " times." << std::endl;
+
+    return 0;
+}
